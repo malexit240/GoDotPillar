af38c58e62b4439fc9f0cc41572db51b6f433d0a
oleksii_mialo <oleksii.mialo@headworks.com.ua>
2025-03-19 19:36:44 +0200

    added offset to polygon

--- GoDotPillar/Extensions/2D/SpriteGDPBetaExtension.cs
+++ GoDotPillar/Extensions/2D/SpriteGDPBetaExtension.cs
@@ -2,12 +2,43 @@
 
 public static class SpriteGDPBetaExtension
 {
-    public static Collections.Array<Vector2[]> ToPolygons(this Sprite2D sprite, float simplification = 2f)
+    public static Collections.Array<Vector2[]> ToPolygons(this Sprite2D sprite, float simplification = 5f)
     {
         var bitMap = new Bitmap();
         bitMap.CreateFromImageAlpha(sprite.Texture.GetImage());
+        var imageSize = bitMap.GetSize();
 
-        return bitMap.OpaqueToPolygons(new Rect2I(Vector2I.Zero, bitMap.GetSize()), simplification);
+        var imageOffset = sprite.Centered ? sprite.Offset - new Vector2I(imageSize.X / 2, imageSize.Y / 2)
+            : sprite.Offset;
+
+        var polygons = bitMap.OpaqueToPolygons(new Rect2I(Vector2I.Zero, imageSize),
+            simplification);
+
+        var correctedPolygons = new Godot.Collections.Array<Vector2[]>
+        {
+        };
+
+        for (int i = 0; i < polygons.Count; i++)
+        {
+            var newPolygon = new Vector2[polygons[i].Length];
+
+            for (int j = 0; j < polygons[i].Length; j++)
+            {
+                var newX = sprite.FlipH
+                    ? -(polygons[i][j].X + imageOffset.X)
+                    : polygons[i][j].X + imageOffset.X;
+
+                var newY = sprite.FlipV
+                    ? -(polygons[i][j].Y + imageOffset.Y)
+                    : polygons[i][j].Y + imageOffset.Y;
+
+                newPolygon[j] = new Vector2(newX, newY);
+            }
+
+            correctedPolygons.Add(newPolygon);
+        }
+
+        return correctedPolygons;
     }
 
     public static Polygon2D ToPolygon2D(this Collections.Array<Vector2[]> source)
