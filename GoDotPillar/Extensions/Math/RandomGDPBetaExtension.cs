dbdf08bc3d4eade44662ac868298e6d1bb8cd483
oleksii_mialo <oleksii.mialo@headworks.com.ua>
2025-04-01 13:22:20 +0300

    added few methods for random getting elements from array or enum

--- GoDotPillar/Extensions/Math/RandomGDPBetaExtension.cs
+++ GoDotPillar/Extensions/Math/RandomGDPBetaExtension.cs
@@ -1,9 +1,24 @@
-﻿namespace Godot;
+﻿using GoDotPillar;
+
+namespace Godot;
 
 public static class RandomGDPBetaExtension
 {
     public static T GetRandomElement<T>(this T[] arr)
     {
         return arr[Random.Shared.Next(arr.Length)];
+    }
+
+    public static T GetRandomElement<T>(this T[] arr, float[] weights)
+    {
+        return RandomGp.GetOption(arr, weights);
+    }
+
+    public static E GetRandomType<E>(this E @enum, float[] weights = default)
+        where E : struct, Enum
+    {
+        return weights == default
+            ? RandomGp.GetOption(Enum.GetValues<E>())
+            : RandomGp.GetOption(Enum.GetValues<E>(), weights);
     }
 }
--- GoDotPillar/Helpers/RandomGp.cs
+++ GoDotPillar/Helpers/RandomGp.cs
@@ -13,4 +13,35 @@ public static class RandomGp
         var dif = maxValue - minValue;
         return (int)(Random.Shared.NextDouble() * dif + minValue);
     }
+
+    public static T GetOption<T>(T[] options, float[] weights)
+    {
+        if (options.Length != weights.Length)
+        {
+            throw new IndexOutOfRangeException("options length and weights length must be equal");
+        }
+
+        var rnd = Random.Shared.NextSingle() * weights.Sum();
+
+        for (int i = 0; i < options.Length; i++)
+        {
+            if (rnd <= weights[i])
+            {
+                return options[i];
+            }
+            else
+            {
+                rnd -= weights[i];
+            }
+        }
+
+        return options[^1];
+    }
+
+    public static T GetOption<T>(T[] options)
+    {
+        var weights = options.Select(o => 1f).ToArray();
+
+        return GetOption<T>(options, weights);
+    }
 }
